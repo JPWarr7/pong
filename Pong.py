61656f15fb651e91c951496c94eba03c5990582f
jonathanpwarren <59145673+jonathanpwarren@users.noreply.github.com>
2020-03-25 09:06:36 -0400

    Update Pong.py

--- Pong.py
+++ Pong.py
@@ -1,9 +1,8 @@
-# Programmed by JPWarren
 # Pong
 import turtle
 
 wn = turtle.Screen()
-wn.title("Pong by JPWarren")
+wn.title("Pong")
 wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
